97c3007180f385661573e4932a06b208da47d12e
Jake 'Donkey" Torrance <jaket1234@hotmail.com>
2018-08-30 09:40:13 +0100

    Add readme, setup.py files and requirements.txt

---
+++ project-b/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='project_b',
+    version='0.1dev',
+    packages=[find_packages()],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
